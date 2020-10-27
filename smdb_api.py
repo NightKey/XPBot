926c6e643aa40e5bba52ae4649a11a6c39f2700b
Night Key <davidjantho@gmail.com>
2020-10-27 23:14:02 +0100

    Created a working version

---
+++ smdb_api.py
@@ -0,0 +1,254 @@
+from sys import getsizeof
+import os, sys, select, socket, json, threading
+from time import sleep, process_time
+
+class ValidationError(Exception):
+    """Get's raised, when validation failes"""
+    def __init__(self, reason):
+        self.message=f"Validation failed! Reason: {reason}"
+
+class NotValidatedError(Exception):
+    """Get's raised, when trying to communicate with the API server, without validation."""
+    def __init__(self):
+        self.message="Can't communicate without validation!"
+
+class ActionFailed(Exception):
+    """Get's raised, when an action failes"""
+    def __init__(self, action):
+        self.message = f"'{action}' failed!'"
+
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+NOTHING = 0
+USER_INPUT = 1
+SENDER = 2
+CHANNEL = 4
+
+class API:
+    """API for the 'Server monitoring Discord bot' application."""
+    def __init__(self, name, key, ip="127.0.0.1", port=9600):
+        """Initialises an API that connects to the 'ip' ip and to the 'port' port with the 'name' name and the 'key' api key
+        """
+        self.ip = ip
+        self.port = port
+        self.name = name
+        self.key =  key
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.valid = False
+        self.call_list = {}
+        self.buffer = []
+        self.sending = False
+        self.running = True
+        self.connection_alive = True
+        self.socket_list = []
+        self.created_function_list=[]
+
+    def _send(self, msg):
+        """Sends a socket message
+        """
+        msg = json.dumps(msg)
+        while True:
+            tmp = ''
+            if len(msg) > 9:
+                tmp = msg[9:]
+                msg = msg[:9]
+            self.socket.send(str(len(msg)).encode(encoding='utf-8'))
+            self.socket.send(msg.encode(encoding="utf-8"))
+            if tmp == '': tmp = '\n'
+            if msg == '\n': break
+            msg = tmp
+
+    def _retrive(self):
+        """Retrives a socket message
+        """
+        ret = ""
+        try:
+            while True: 
+                blockPrint()
+                size = int(self.socket.recv(1).decode('utf-8'))
+                data = self.socket.recv(size).decode('utf-8')
+                enablePrint()
+                if data == '\n': break
+                ret += data
+            return json.loads(ret)
+        except Exception as ex:
+            enablePrint()
+            print(ex)
+            return None
+    
+    def _re_init_commands(self):
+        while not self.connection_alive:
+            pass
+        from copy import deepcopy
+        tmp = deepcopy(self.created_function_list)
+        for item in tmp:
+            self.create_function(*item)
+        del self.tmp
+        del tmp
+
+    def validate(self, timeout=None):
+        """Validates with the bot, and starts the listener loop, if validation is finished.
+        Time out can be set, so it won't halt the program for ever, if no bot is present. (The timeout will only work for the first validation.)
+        """
+        start = process_time()
+        while True:
+            try:
+                self.socket.connect((self.ip, self.port))
+                break
+            except ConnectionRefusedError: pass
+            if timeout is not None and process_time() - start > timeout:
+                return False
+        self._send(self.name)
+        self._send(self.key)
+        ansvear = self._retrive()
+        if ansvear == 'Denied':
+            reason = self._retrive()
+            raise ValidationError(reason)
+        elif ansvear == None:
+            raise ValueError("Bad value retrived from socket.")
+        else:
+            self.valid = True
+            self.socket_list.append(self.socket)
+            if self.connection_alive:
+                self.th = threading.Thread(target=self._listener)
+                self.th.name = "Listener Thread"
+                self.th.start()
+            if not self.connection_alive:
+                self.tmp = threading.Thread(target=self._re_init_commands)
+                self.tmp.start()
+                
+
+    def get_status(self):
+        """Gets the bot's status
+        """
+        if self.valid:
+            self.sending = True
+            self._send("Status")
+            while self.buffer == []:
+                sleep(0.1)
+            tmp = self.buffer[0]
+            self.buffer = []
+            self.sending = False
+            return tmp
+        else: raise NotValidatedError()
+    
+    def get_username(self, key):
+        self.sending = True
+        self._send('Username')
+        self._send(key)
+        while self.buffer == []:
+            sleep(0.1)
+        self.sending = False
+        tmp = self.buffer[0]
+        self.buffer = []
+        return tmp if tmp is not None else "unknown"
+
+    def send_message(self, message, destination=None):
+        """Sends a message trough the discord bot.
+        """
+        if self.valid:
+            self.sending = True
+            self._send("Send")
+            self._send([message, destination])
+            while self.buffer == []:
+                sleep(0.1)
+            tmp = self.buffer[0]
+            self.buffer = []
+            self.sending = False
+            if not tmp: raise ActionFailed("Send message")
+        else: raise NotValidatedError()
+
+    def _listener(self):
+        """Listens for incoming messages, and stops when the program stops running
+        """
+        retrived_call=[]
+        while self.running:
+            while self.valid and self.connection_alive:
+                try:
+                    read_socket, _, exception_socket = select.select(self.socket_list, [], self.socket_list)
+                    if read_socket != []:
+                        msg = self._retrive()
+                        if self.sending:
+                            self.buffer.append(msg)
+                        elif msg in self.call_list:
+                            retrived_call.append(msg)
+                        elif msg is None and retrived_call != []:
+                            call = threading.Thread(target=self.call_list[retrived_call[0]], args=[*retrived_call[1:], ])
+                            call.name = self.call_list[retrived_call[0]]
+                            retrived_call = []
+                            call.start()
+                        elif retrived_call != []:
+                            retrived_call.append(msg)
+                        elif msg is None:
+                            self.connection_alive = False
+                            self.socket.close()
+                            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            self.socket_list = []
+                    if exception_socket != []:
+                        self.connection_alive = False
+                        self.socket.close()
+                        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        self.socket_list = []
+                except: pass
+
+            try:
+                if self.running: 
+                    self.validate()
+                    self.connection_alive = True
+            except Exception as ex: print(f"{type(ex)} -> {ex}")
+
+    def close(self, reason=None):
+        """Closes the socket, and stops the listener loop.
+        """
+        self.running = False
+        self.valid = False
+        self.connection_alive = False
+        self._send("Disconnect")
+        self._send(reason)
+        self.socket.close()
+
+    def create_function(self, name, help_text, callback, return_value=[NOTHING]):
+        """Creates a function in the connected bot.
+        Return order: ChannelID, UserID, UserInput. The returned value depends on the return value, but the order is the same.
+        """
+        if self.valid:
+            if isinstance(return_value, (tuple, list)):
+                self.created_function_list.append([name, help_text, callback, sum(return_value)])
+                return_value = sum(return_value)
+            self.sending = True
+            self._send("Create")
+            self._send([name, help_text, name, return_value])
+            while self.buffer == []:
+                sleep(0.1)
+            tmp = self.buffer[0]
+            self.buffer = []
+            self.sending = False
+            if tmp:
+                self.call_list[name] = callback
+            else: raise ActionFailed("Create")
+        else: raise NotValidatedError()
+
+if __name__ == "__main__":
+    api = API("Test", "80716cbfd9f90428cd308acc193b4b58519a4f10a7440b97aaffecf75e63ecec")
+    input("Press return to start...")
+    api.validate()
+    print('Validation finished')
+    print(api.get_status())
+    print('Status finished')
+    api.send_message("Test", destination="165892968283242497")
+    print('Message finished')
+    def sst(usr, msg):
+        print(api.get_username(usr))
+        print(msg)
+        if msg == "": msg = "Empty message"
+        api.send_message(msg, usr)
+    api.create_function("SuperSecretTest",
+    "It's a super secret test option!\nUsage: &SuperSecretTest <You can say aaaanything>\nCategory: SOFTWARE",
+    sst, [USER_INPUT, SENDER])
+    print('Function created')
+    input("Press return to exit")
+    api.close("it's the end of the word!")
---
+++ xpbot.py
@@ -0,0 +1,208 @@
+import smdb_api as API
+import json, threading
+from time import sleep, time
+from os import path, walk, mkdir
+from fuzzywuzzy import fuzz
+
+class subject:
+    def __init__(self, name, username, lvl_up_callback):
+        self.name = name
+        self.username = username
+        self.started_at = None
+        self.xp = 50
+        self.xp_increment = 1
+        self.lvl = 0
+        self.started = False
+        self._learning = None
+        self.lvl_up_callback = lvl_up_callback
+
+    @classmethod
+    def load(cls, name, username, lvl_up_callback):
+        try:
+            with open(path.join("subjects", f"{name}_{username}.json"), 'r') as f:
+                tmp = json.load(f)
+            ret = cls(tmp["name"], tmp["username"], lvl_up_callback)
+            ret.xp = tmp['xp']
+            ret.lvl = tmp['lvl']
+            ret.xp_increment = tmp['xp_increment']
+            return ret
+        except:
+            return None
+
+    def start(self):
+        self.started_at = int(time())
+        self.started = True
+        self._learning = threading.Thread(target=self.learning)
+        self._learning.name = f"{self.username} learning {self.name}"
+        self._learning.start()
+
+    def save(self):
+        tmp = {'name':self.name, 'username':self.username, 'xp':self.xp, 'lvl': self.lvl, 'xp_increment':self.xp_increment}
+        with open(path.join("subjects", f"{self.name}_{self.username}.json"), 'w') as f:
+            json.dump(tmp, f)
+
+    def increase_xp(self, bonus=1):
+        self.xp += self.xp_increment*bonus
+        if self.xp == 100*(1+self.lvl):
+            self.lvl += 1
+            self.xp = 0
+            self.xp_increment += 0.5
+            self.lvl_up_callback(self.lvl, self.name)
+
+    def return_stats(self):
+        ret = f"{self.name}: {self.lvl} lvl\n"
+        ret += f"{100*(1+self.lvl) - self.xp} xp to the next lvl.{' In progress' if self.started else ''}\n"
+        return ret
+
+    def learning(self):
+        last_hour = last_minute = last_sec = self.started_at
+        while self.started:
+            now = int(time())
+            if now - last_sec >= 30:
+                self.increase_xp()
+                last_sec = now
+            if now - last_minute >= 1800:
+                self.increase_xp(1.01)
+                last_minute = now
+            if now - last_hour >= 3600:
+                self.increase_xp(1.02)
+                last_hour = now
+            sleep(1)
+        delta = ((now - self.started_at % 3600) % 1800 )% 30
+        self.xp += (self.xp_increment/30) * delta
+    
+    def stop(self):
+        self.stared = False
+
+class user:
+    def __init__(self, id, name, subjects, client):
+        self.id = id
+        self.name = name
+        self.client = client
+        self.subjects = {}
+        if isinstance(subjects, str):
+            for _subject in subjects.split(' '):
+                self.subjects[_subject] = subject(_subject, name, self.lvl_up_callback)
+        else:
+            for _subject in subjects:
+                self.subjects[_subject] = subject.load(_subject, name, self.lvl_up_callback)
+        self.save()
+
+    @classmethod
+    def load(cls, name, client):
+        if name.endswith('.json'):
+            with open(path.join("data", f"{name}"), 'r') as f:
+                try:
+                    tmp = json.load(f)
+                except:
+                    return None
+            return cls(tmp["id"], tmp["name"], tmp["subjects"], client)
+        return None
+
+    def searc_subject(self, subject):
+        mx = {}
+        for key in self.subjects.keys():
+            tmp = fuzz.ratio(subject.lower(), key.lower())
+            if 'value' not in mx or mx["value"] < tmp:
+                    mx["key"] = key
+                    mx["value"] = tmp
+        if mx['value'] > 90:
+            return mx['key'], True
+        else:
+            return mx['key'], False
+
+    def start(self, subject):
+        if subject not in self.subjects:
+            ret = self.searc_subject(subject)
+            if ret[1]:
+                subject = ret[0]
+            else:
+                self.client.send_message(f"You probably wanted {ret[0]}.", destination=self.id)
+                return
+        self.subjects[subject].start()
+        self.client.send_message(f"Started {subject}.", destination=self.id)
+
+    def lvl_up_callback(self, lvl, subject_name):
+        self.client.send_message(f"You reached lvl {lvl} in {subject_name}!", destination=self.id)
+    
+    def add_subject(self, _subject):
+        if subject is not None:
+            self.subjects[_subject] = subject(_subject, self.name, self.lvl_up_callback)
+        self.client.send_message(f"Added {_subject} to your subjects!", destination=self.id)
+
+    def stop(self, subject):
+        if subject not in self.subjects:
+            ret = self.searc_subject(subject)
+            if ret[1]:
+                subject = ret[0]
+            else:
+                self.client.send_message(f"You probably wanted {ret[0]}.", destination=self.id)
+                return
+        self.subjects[subject].stop()
+        self.client.send_message(f"Stopped {subject}.", destination=self.id)
+        self.save()
+
+    def get_status(self, subject):
+        to_send = "```\n"
+        if subject != "":
+            to_send += self.subjects[subject].return_stats()
+        else:
+            for subject in self.subjects.values():
+                to_send += subject.return_stats()
+        to_send += "```"
+        self.client.send_message(to_send, destination=self.id)
+
+    def save(self):
+        tmp = {}
+        tmp["id"] = self.id
+        tmp["name"] = self.name
+        tmp["subjects"] = list(self.subjects.keys())
+        with open(path.join("data", f"{self.name}.json"), "w") as f:
+            json.dump(tmp, f)
+        for subject in self.subjects.values():
+            subject.save()
+
+users = {}
+client = None
+
+def create_profile(userID, subjects):
+    name = client.get_username(userID)
+    users[userID] = user(userID, name, subjects, client)
+
+def start(userID, subject):
+    users[userID].start(subject)
+
+def stop(userID, subject):
+    users[userID].stop(subject)
+
+def XPStatus(userID, subject):
+    users[userID].get_status(subject)
+
+def load():
+    for _, _, names, in walk("data"):
+        for name in names:
+            tmp = user.load(name, client)
+            if tmp is not None:
+                users[tmp.id] = tmp
+
+def add_subject(userID, subject):
+    users[userID].add_subject(subject)
+
+if __name__ == "__main__":
+    if not path.exists("data"):
+        mkdir("data")
+    if not path.exists("subject"):
+        mkdir("subject")
+    client = API.API("XPBot", "19927a7bf1dbf16d70d6dad81ed5a45ef60ab6f41645c68feaa3b870e3e9c65a")
+    load()
+    client.validate()
+    client.create_function("createprofile", "Creates a profile, with the given subjects.\nUsage: &createprofile [subject1 subject2 ... subjectx]\nCategory: USER",
+                            create_profile, return_value=[API.USER_INPUT, API.SENDER])
+    client.create_function("start", "Starts the learning of the selected subject.\nUsage: &start <subject1>\nCategory: USER",
+                            start, return_value=[API.USER_INPUT, API.SENDER])
+    client.create_function("stop", "Stops the learning of the selected subject.\nUsage: &stop <subject1>\nCategory: USER",
+                            stop, return_value=[API.USER_INPUT, API.SENDER])
+    client.create_function("addSubject", "Adds a subject to your list.\nUsage: &addSubject <subject1>\nCategory: USER",
+                            add_subject, return_value=[API.USER_INPUT, API.SENDER])
+    client.create_function("XPStatus", "Returns either the selected subject's status, or the useres current status for every subject.\nUsage: &XPStatus <optional subject1>\nCategory: USER",
+                            XPStatus, return_value=[API.USER_INPUT, API.SENDER])

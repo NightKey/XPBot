e4478965eee468dab434d60841fe2e0bb8ae0252
Night Key <davidjantho@gmail.com>
2020-10-28 10:31:01 +0100

    Fixed an issue, with the file creation being bad

--- xpbot.py
+++ xpbot.py
@@ -126,9 +126,12 @@ class user:
         self.client.send_message(f"You reached lvl {lvl} in {subject_name}!", destination=self.id)
     
     def add_subject(self, _subject):
-        if subject is not None:
+        if _subject != "":
             self.subjects[_subject] = subject(_subject, self.name, self.lvl_up_callback)
-        self.client.send_message(f"Added {_subject} to your subjects!", destination=self.id)
+            self.subjects[_subject].save()
+            self.client.send_message(f"Added {_subject} to your subjects!", destination=self.id)
+        else:
+            self.client.send_message("You need to specify the subject you want to add!", destination=self.id)
 
     def stop(self, subject):
         if subject not in self.subjects:
@@ -151,6 +154,17 @@ class user:
                 to_send += subject.return_stats()
         to_send += "```"
         self.client.send_message(to_send, destination=self.id)
+
+    def remove_subject(self, subject):
+        if subject not in self.subjects:
+            ret = self.searc_subject(subject)
+            if ret[1]:
+                subject = ret[0]
+            else:
+                self.client.send_message(f"You probably wanted {ret[0]}.", destination=self.id)
+                return
+        del self.subjects[subject]
+        self.client.send_message(f"{subject} removed!", destination=self.id)
 
     def save(self):
         tmp = {}
@@ -188,11 +202,14 @@ def load():
 def add_subject(userID, subject):
     users[userID].add_subject(subject)
 
+def remove_subject(userID, subject):
+    users[userID].remove_subject(subject)
+
 if __name__ == "__main__":
     if not path.exists("data"):
         mkdir("data")
-    if not path.exists("subject"):
-        mkdir("subject")
+    if not path.exists("subjects"):
+        mkdir("subjects")
     client = API.API("XPBot", "19927a7bf1dbf16d70d6dad81ed5a45ef60ab6f41645c68feaa3b870e3e9c65a")
     load()
     client.validate()
@@ -202,7 +219,9 @@ if __name__ == "__main__":
                             start, return_value=[API.USER_INPUT, API.SENDER])
     client.create_function("stop", "Stops the learning of the selected subject.\nUsage: &stop <subject1>\nCategory: USER",
                             stop, return_value=[API.USER_INPUT, API.SENDER])
-    client.create_function("addSubject", "Adds a subject to your list.\nUsage: &addSubject <subject1>\nCategory: USER",
+    client.create_function("addsubject", "Adds a subject to your list.\nUsage: &addsubject <subject1>\nCategory: USER",
                             add_subject, return_value=[API.USER_INPUT, API.SENDER])
+    client.create_function("removesubject", "Removes a subject from your list.\nUsage: &removesubject <subject1>\nCategory: USER",
+                            remove_subject, return_value=[API.USER_INPUT, API.SENDER])
     client.create_function("XPStatus", "Returns either the selected subject's status, or the useres current status for every subject.\nUsage: &XPStatus <optional subject1>\nCategory: USER",
                             XPStatus, return_value=[API.USER_INPUT, API.SENDER])
